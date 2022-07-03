5b41dee13c6eddad2f7cdda5363e6df570ea2e0b
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-03 14:37:14 +0200

    2 files modified

--- src/app/teatro/teatro.component.ts
+++ src/app/teatro/teatro.component.ts
@@ -5,7 +5,10 @@ import { Teatro } from '../app.component';
 export class Pulsante {
   pulsante;
   prenota;
-  constructor(nome) {}
+  nome;
+  constructor(nome) {
+    this.nome = nome;
+  }
 }
 export class Prenotazione {
   zona: string;
@@ -43,18 +46,21 @@ export class Selezione {
 export class TeatroComponent implements OnInit {
   @Input() teatro$;
   sub: Subscription;
-  platea: Array<Array<string>>;
+  platea;
   palco: Array<Array<string>>;
   nomeUtente: string;
   constructor() {}
-  prenota(nome, fila, posto) {
-    console.log(this.platea);
+  prenota($event) {
+    console.log($event);
   }
   ngOnInit() {
     this.sub = this.teatro$.subscribe((teatro: Teatro) => {
+      this.platea = teatro.platea.map((fila) =>
+        fila.map((posto) => new Pulsante(posto))
+      );
+
       this.platea = teatro.platea;
       this.palco = teatro.palco;
-      this.platea.map((fila) => fila.map((posto) => new Pulsante(posto)));
     });
   }
 }
