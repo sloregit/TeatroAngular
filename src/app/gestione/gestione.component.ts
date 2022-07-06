8cab29fd67a61b0d98ceed59eff6a6750c518e56
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-07 00:08:49 +0200

    1 file modified

--- src/app/gestione/gestione.component.ts
+++ src/app/gestione/gestione.component.ts
@@ -3,34 +3,12 @@ import { Observable, of, map, Subscription } from 'rxjs';
 import { Teatro } from '../app.component';
 import { TeatroDBService } from '../teatro-db.service';
 
-@Component({
-  selector: 'app-gestione',
-  templateUrl: './gestione.component.html',
-  styleUrls: ['./gestione.component.css'],
-})
-export class GestioneComponent implements OnInit {
+export class GestoreTeatro {
   teatro: Teatro;
-  filePlateaMax: Array<string>;
-  postiPlateaMax: Array<string>;
-  filePalchiMax: Array<string>;
-  postiPalchiMax: Array<string>; 
-  sub: Subscription;
-  key: string;
-  newKey: string;
-  conferma: string;
-  constructor(private TeatroDBservice: TeatroDBService) {
-    this.filePlateaMax = new Array(7);
-    this.postiPlateaMax = new Array(10);
-    this.filePalchiMax = new Array(6);
-    this.postiPalchiMax = new Array(4);
-  }
-  nuovaChiave() {
-    this.TeatroDBservice.getNewKey$().subscribe(
-      (chiave: string) => (this.newKey = chiave)
-    );
+  constructor() {
+    this.teatro = new Teatro();
   }
   aggiungiTeatro(filePlatea, postiPlatea, filePalco, postipalco) {
-    this.teatro = new Teatro();
     this.teatro.platea = Array(filePlatea * 1)
       .fill('fila')
       .map(() =>
@@ -49,12 +27,46 @@ export class GestioneComponent implements OnInit {
             return undefined;
           })
       );
+  }
+}
+
+@Component({
+  selector: 'app-gestione',
+  templateUrl: './gestione.component.html',
+  styleUrls: ['./gestione.component.css'],
+})
+export class GestioneComponent implements OnInit {
+  gestore: GestoreTeatro;
+  filePlateaMax: Array<string>;
+  postiPlateaMax: Array<string>;
+  filePalchiMax: Array<string>;
+  postiPalchiMax: Array<string>;
+  sub: Subscription;
+  key: string;
+  newKey: string;
+  conferma: string;
+  constructor(private TeatroDBservice: TeatroDBService) {
+    this.filePlateaMax = new Array(7);
+    this.postiPlateaMax = new Array(10);
+    this.filePalchiMax = new Array(6);
+    this.postiPalchiMax = new Array(4);
+  }
+  //genera una nuova chiave
+  nuovaChiave() {
+    this.TeatroDBservice.getNewKey$().subscribe(
+      (chiave: string) => (this.newKey = chiave)
+    );
+  }
+  //Genera un nuovo teatro e lo inserisce in corrispondenza della chiave;
+  aggiungiTeatro(filePlatea, postiPlatea, filePalco, postipalco) {
+    this.gestore = new GestoreTeatro();
+    this.gestore.aggiungiTeatro(filePlatea, postiPlatea, filePalco, postipalco);
     this.TeatroDBservice.SetPrenotazioni$(
       this.key,
-      JSON.stringify(this.teatro)
+      JSON.stringify(this.gestore.teatro)
     ).subscribe({
       next: (conf) =>
-        (this.conferma = conf + 'Teatro aggiunto, Chiave: ' + this.key),
+        (this.conferma = conf + ': Teatro aggiunto, Chiave: ' + this.key),
       error: (err) => console.error('Errore in SetPrenotazioni$: ' + err),
     });
   }
