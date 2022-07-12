9efdf3aa07dac6c04a5da1974e5dd28777457b60
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-12 12:11:31 +0200

    1 file modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TeatroDBService } from './teatro-db.service';
 describe('Testing tests', () => {
   it('should succeed', () => expect(true).toEqual(true));
   it('should fail', () => expect(true).toEqual(false));
