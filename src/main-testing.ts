fd2956f922d1caf7bf1abce8c94488cb1942ebb6
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-12 15:44:32 +0200

    1 file modified

--- src/main-testing.ts
+++ src/main-testing.ts
@@ -1,6 +1,6 @@
 import './global-jasmine';
 import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
-import 'jasmine-core/lib/jasmine-core/boot0.js';
+import 'jasmine-core/lib/jasmine-core/boot.js';
 
 import './polyfills';
 
