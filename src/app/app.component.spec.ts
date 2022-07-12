c8e268cde5616018935afa0795a1919ca60f25ee
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-12 15:23:46 +0200

    1 file modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,44 @@ import { TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TeatroDBService } from './teatro-db.service';
+
 describe('Testing tests', () => {
-  it('should succeed', () => expect(true).toEqual(true));
-  it('should fail', () => expect(true).toEqual(false));
+  it('Vero = vero', () => expect(true).toBeTrue());
+  it('Vero = falso', () => expect(true).toBeFalse());
 });
+/*
+describe('AppComponent', () => {
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientModule],
+        providers: [AppComponent],
+      }).compileComponents();
+    })
+  );
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeDefined();
+  });
+
+  it(`should have as title 'Temperature in Angular 11'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Temperature in Angular 11');
+  });
+
+  it("l'oggetto selezione dovrebbe essere indefinita", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.selezione).toBeUndefined();
+  });
+
+  it("l'array cities dovrebbe contenere la stringa inserita con addCity", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    app.addCity('Verona');
+    expect(app.cities).toContain('Verona');
+  });
+});*/
