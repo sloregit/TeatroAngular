fdd507e705a33d4652e8951743f033e485a98503
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-07-12 12:06:46 +0200

    3 files modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,6 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 describe('Testing tests', () => {
   it('should succeed', () => expect(true).toEqual(true));
   it('should fail', () => expect(true).toEqual(false));
